6aea30d789cd048e784e34959907ead60bfe2ffa
Dika Afty <dikaafty@gmail.com>
2025-08-07 21:36:24 +0700

    Delete revenue prop of RevenueChart component

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -9,11 +9,7 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
