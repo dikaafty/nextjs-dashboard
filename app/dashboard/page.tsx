eb886d2a7bbc2863d3a52a933f280521322f8bcb
Dika Afty <dikaafty@gmail.com>
2025-08-07 13:24:03 +0700

    Import LatestInvoices component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { Card } from "../ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 
 export default function Page() {
   return <p>Dashboard Page</p>;
