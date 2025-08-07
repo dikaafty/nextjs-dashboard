0ba9fe05a8ba9923ec96b97fd5b0914548600bb0
Dika Afty <dikaafty@gmail.com>
2025-08-07 21:52:50 +0700

    Render CardWrapper component in Page component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -18,7 +18,7 @@ export default async function Page() {
       </h1>
 
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        
+        <CardWrapper />
       </div>
 
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
