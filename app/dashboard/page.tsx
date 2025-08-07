600712b4d852108836e414cd5195d905ff114038
Dika Afty <dikaafty@gmail.com>
2025-08-07 13:24:34 +0700

    Import lusitana font

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { Card } from "../ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
+import { lusitana } from "../ui/fonts";
 
 export default function Page() {
   return <p>Dashboard Page</p>;
