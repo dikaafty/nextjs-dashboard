60f0784fa207444e871e6920b1621edbbe8e83c8
Dika Afty <dikaafty@gmail.com>
2025-08-07 21:50:45 +0700

    Delete fetchCardData function import

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,6 @@ import { Card } from "../../ui/dashboard/cards";
 import RevenueChart from "../../ui/dashboard/revenue-chart";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import { lusitana } from "../../ui/fonts";
-import { fetchCardData } from "../../lib/data";
 import { Suspense } from "react";
 import { RevenueChartSkeleton, LatestInvoicesSkeleton } from "@/app/ui/skeletons";
 
