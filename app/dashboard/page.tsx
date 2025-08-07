67eb1a967c24c2e5cebb1e1a664325e033dbfe3a
Dika Afty <dikaafty@gmail.com>
2025-08-07 13:51:55 +0700

    Import fetchCardData function

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { Card } from "../ui/dashboard/cards";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import { lusitana } from "../ui/fonts";
-import { fetchRevenue, fetchLatestInvoices } from "../lib/data";
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
