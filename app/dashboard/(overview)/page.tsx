d5a579ad7ea57d7e75057a0fef1f6c6f95f9bb26
Dika Afty <dikaafty@gmail.com>
2025-08-07 21:10:18 +0700

    Delete revenue variable in Page component

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,6 @@ import { lusitana } from "../../ui/fonts";
 import { fetchLatestInvoices, fetchCardData } from "../../lib/data";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const { 
     totalPaidInvoices, totalPendingInvoices,
