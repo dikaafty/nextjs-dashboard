3a8948214562285e8ef811cd627c53db71bcef30
Dika Afty <dikaafty@gmail.com>
2025-08-08 14:16:45 +0700

    Import CreateInvoice component

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,6 +1,7 @@
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
 
 export default function Page() {
   return <p>Invoices Page</p>;
