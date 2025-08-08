9180583be6524d3b8862c6e0d263dfaffa850248
Dika Afty <dikaafty@gmail.com>
2025-08-08 14:17:04 +0700

    Import lusitana font

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,6 +2,7 @@ import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
 
 export default function Page() {
   return <p>Invoices Page</p>;
