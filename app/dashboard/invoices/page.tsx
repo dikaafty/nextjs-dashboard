52ec699d3b93853168346f2a7eba535c14b6ecd8
Dika Afty <dikaafty@gmail.com>
2025-08-08 14:16:16 +0700

    Import Table component

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,6 @@
 import Pagination from "@/app/ui/invoices/pagination";
 import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
 
 export default function Page() {
   return <p>Invoices Page</p>;
