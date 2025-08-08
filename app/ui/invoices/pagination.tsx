54c1b39bdfc445293c5a7fb9dced9c5d36aaf525
Dika Afty <dikaafty@gmail.com>
2025-08-08 23:56:37 +0700

    Create createPageURL function and its logic in Pagination component

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -12,6 +12,12 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const currentPage = Number(searchParams.get('page')) || 1;
   const allPages = generatePagination(currentPage, totalPages);
 
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
   return (
     <>
       <div className="inline-flex">
