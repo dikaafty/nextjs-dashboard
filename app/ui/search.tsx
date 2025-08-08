5e4397f1db2412652a346d96fb042f91124ec41f
Dika Afty <dikaafty@gmail.com>
2025-08-08 23:31:30 +0700

    Wrap content of handleSearch function with useDebouncedCallback hook in Search component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,7 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParam);
 
     if(term) {
@@ -19,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300)
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
