bc1b876dd8e9ad3ddff53b0685fe8f0a1afe1678
Dika Afty <dikaafty@gmail.com>
2025-08-08 14:17:31 +0700

    Import InvoicesTableSkeleton component

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,6 +3,7 @@ import Search from "@/app/ui/search";
 import Table from "@/app/ui/invoices/table";
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 
 export default function Page() {
   return <p>Invoices Page</p>;
