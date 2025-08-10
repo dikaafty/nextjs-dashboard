efe23e9969f722102c9ff3def6d62e1305db7951
Dika Afty <dikaafty@gmail.com>
2025-08-10 14:27:58 +0700

    Create and export Page component

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
+
+export default function Page() {
+  
+}
