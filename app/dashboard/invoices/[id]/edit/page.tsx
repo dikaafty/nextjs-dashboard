cd9650de94c2d64a7ebb27adccc80de99148a7ca
Dika Afty <dikaafty@gmail.com>
2025-08-11 20:56:39 +0700

    Create condition to invoke notFound function in Page component

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -11,6 +11,10 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchCustomers(),
   ]);
 
+  if(!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
