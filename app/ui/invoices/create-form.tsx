888f3c05667f104ac6dce45b4f665ae2f13478dd
Dika Afty <dikaafty@gmail.com>
2025-08-11 21:37:44 +0700

    Setup destructuring using useActionState hook in Form component

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -14,6 +14,7 @@ import { useActionState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
    const initialState: State = { message: null, errors: {} };
+   const [state, formAction] = useActionState(createInvoice, initialState);
   
   return (
     <form action={createInvoice}>
